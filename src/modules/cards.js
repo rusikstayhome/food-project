41477bc90a140d34301fdb9c2b3b21c9b5a6b848
rusikstayhome <alenrus32@gmail.com>
2022-08-04 14:52:42 +0300

    fixed bags and build the project with webstorm

--- src/modules/cards.js
+++ src/modules/cards.js
@@ -1,8 +1,4 @@
-import { getResource } from '../services/services';
-
 function cards() {
-    // Используем классы для создание карточек меню
-
     class MenuCard {
         constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
@@ -31,25 +27,35 @@ function cards() {
             }
 
             element.innerHTML = `
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                </div>
-            `;
+                  <img src=${this.src} alt=${this.alt}>
+                  <h3 class="menu__item-subtitle">${this.title}</h3>
+                  <div class="menu__item-descr">${this.descr}</div>
+                  <div class="menu__item-divider"></div>
+                  <div class="menu__item-price">
+                      <div class="menu__item-cost">Цена:</div>
+                      <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                  </div>
+              `;
             this.parent.append(element);
         }
     }
 
-    getResource('http://localhost:3000/menu')
+    const getResources = async (url) => {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    };
+
+    getResources('http://localhost:3000/menu')
         .then(data => {
             data.forEach(({ img, altimg, title, descr, price }) => {
-                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
 }
 
-export default cards;
+module.exports = cards;
